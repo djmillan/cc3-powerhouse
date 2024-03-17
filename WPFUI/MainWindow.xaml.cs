043086ca248456897626d5e1e2ea7c811f5914a8
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-17 13:16:23 +0800

    improve

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+using System;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,42 +20,68 @@ namespace WPFUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private GameSession _gameSession;
+        private readonly GameSession _gameSession;
+        private readonly SoundPlayer _soundPlayer;
+
         public MainWindow()
         {
             InitializeComponent();
 
             _gameSession = new GameSession();
-
             DataContext = _gameSession;
+
+            // Set up the sound player
+            if (!File.Exists(@"C:\cc3-1b-millandj\SOSCSRPG\WPFUI\Music\AMV - Berserk - Forces.wav"))
+            {
+                throw new FileNotFoundException("The specified sound file does not exist.");
+            }
+
+            _soundPlayer = new SoundPlayer(@"C:\cc3-1b-millandj\SOSCSRPG\WPFUI\Music\AMV - Berserk - Forces.wav");
         }
+
+        private void OnMove(object sender, RoutedEventArgs e, Action moveAction)
+        {
+            try
+            {
+                moveAction();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
         private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
         {
-            _gameSession.MoveNorth();
+            OnMove(sender, e, _gameSession.MoveNorth);
         }
+
         private void OnClick_MoveWest(object sender, RoutedEventArgs e)
         {
-            _gameSession.MoveWest();
+            OnMove(sender, e, _gameSession.MoveWest);
         }
+
         private void OnClick_MoveEast(object sender, RoutedEventArgs e)
         {
-            _gameSession.MoveEast();
+            OnMove(sender, e, _gameSession.MoveEast);
         }
+
         private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
         {
-            _gameSession.MoveSouth();
+            OnMove(sender, e, _gameSession.MoveSouth);
         }
-        //Some additional xD
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+
+        private void OnClick_AddExperience(object sender, RoutedEventArgs e)
         {
-            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+            _gameSession.CurrentPlayer.ExperiencePoints += 10;
         }
-        SoundPlayer _soundPlayer = new SoundPlayer(@"C:\cc3-1b-millandj\SOSCSRPG\WPFUI\Music\AMV - Berserk - Forces.wav");
-        private void button1_Click(object sender, RoutedEventArgs e)
+
+        private void OnClick_PlaySound(object sender, RoutedEventArgs e)
         {
             _soundPlayer.Play();
         }
-        private void button2_Click(object sender, RoutedEventArgs e)
+
+        private void OnClick_StopSound(object sender, RoutedEventArgs e)
         {
             _soundPlayer.Stop();
         }
