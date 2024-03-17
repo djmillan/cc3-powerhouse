1febb8e5cf467ac7af8b2a591bf6772bf78358a5
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-17 13:43:50 +0800

    Revert "improve"
    This reverts commit 94545f59ad2a5c2fa15e390ad69201b62e7c4270.

--- Engine/BaseNotificationClass.cs
+++ Engine/BaseNotificationClass.cs
@@ -1,42 +1,18 @@
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Engine
 {
     public class BaseNotificationClass : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Called when a property value changes.
-        /// </summary>
-        /// <param name="propertyName">The name of the property that changed.</param>
         protected virtual void OnPropertyChanged(string propertyName)
         {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        /// <summary>
-        /// Sets the value of a property and raises the PropertyChanged event if the value has changed.
-        /// </summary>
-        /// <typeparam name="T">The type of the property.</typeparam>
-        /// <param name="field">The backing field for the property.</param>
-        /// <param name="value">The new value for the property.</param>
-        /// <param name="propertyName">The name of the property.</param>
-        /// <returns>True if the value was changed, false otherwise.</returns>
-        protected bool SetProperty<T>(ref T field, T value, string propertyName)
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, value))
-            {
-                field = value;
-                OnPropertyChanged(propertyName);
-                return true;
-            }
-
-            return false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
