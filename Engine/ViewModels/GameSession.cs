6109819ec69b3175579ce1a06f82e820602a3202
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-17 13:16:38 +0800

    improve

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,18 +9,19 @@ using Engine.Factories;
 
 namespace Engine.ViewModels
 {
-    //Inheritance for Base
-    public class GameSession : BaseNotificationClass
+    // Inheritance for INotifyPropertyChanged
+    public class GameSession : INotifyPropertyChanged
     {
         private Location _currentLocation;
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation {
-            get { return _currentLocation; }
+
+        public Location CurrentLocation
+        {
+            get => _currentLocation;
             set
             {
                 _currentLocation = value;
-                //Refractored
                 OnPropertyChanged(nameof(CurrentLocation));
                 OnPropertyChanged(nameof(HasLocationToNorth));
                 OnPropertyChanged(nameof(HasLocationToEast));
@@ -28,85 +29,76 @@ namespace Engine.ViewModels
                 OnPropertyChanged(nameof(HasLocationToSouth));
             }
         }
-        public bool HasLocationToNorth
+
+        public bool HasLocationToNorth => CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+
+        public bool HasLocationToEast => CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+
+        public bool HasLocationToWest => CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+
+        public bool HasLocationToSouth => CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+
+        public GameSession()
         {
-            get
+            CurrentPlayer = new Player
             {
-                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
-            }
-        }
-        public bool HasLocationToEast
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
-            }
-        }
-        public bool HasLocationToWest
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
-            }
-        }
-        public bool HasLocationToSouth
-        {
-            get
-            {
-                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
-            }
-        }
-        //Object initializers
-        public GameSession() 
-        { 
-            CurrentPlayer = new Models.Player 
-            {   Name = "Bot", 
-                CharacterClass = "Warrior", 
-                HitPoints = 10, 
-                Gold = 1000000, 
-                ExperiencePoints = 0, 
-                Level = 1 
+                Name = "Bot",
+                CharacterClass = "Warrior",
+                HitPoints = 10,
+                Gold = 1000000,
+                ExperiencePoints = 0,
+                Level = 1
             };
 
             CurrentWorld = WorldFactory.CreateWorld();
             CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
 
-        //Added Guard clauses
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public void MoveNorth()
         {
-            if(HasLocationToNorth)
+            if (CanMoveNorth)
             {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
-
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
             }
         }
+
+        public bool CanMoveNorth => HasLocationToNorth;
+
         public void MoveEast()
         {
-            if(HasLocationToEast)
+            if (CanMoveEast)
             {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
-
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
             }
-
         }
+
+        public bool CanMoveEast => HasLocationToEast;
+
         public void MoveSouth()
         {
-            if(HasLocationToSouth)
+            if (CanMoveSouth)
             {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
-
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
             }
-
         }
+
+        public bool CanMoveSouth => HasLocationToSouth;
+
         public void MoveWest()
         {
-            if(HasLocationToWest)
+            if (CanMoveWest)
             {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
-
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
             }
-
         }
+
+        public bool CanMoveWest => HasLocationToWest;
     }
 }
