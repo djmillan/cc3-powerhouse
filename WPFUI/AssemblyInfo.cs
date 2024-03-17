046dc520e4723d9ab61d5c78c77240cb12da384b
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-17 13:16:17 +0800

    improve

--- WPFUI/AssemblyInfo.cs
+++ WPFUI/AssemblyInfo.cs
@@ -1,10 +1,13 @@
 using System.Windows;
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
-                                                //(used if a resource is not found in the page,
-                                                // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
-                                                //(used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly, // look for generic resources in the calling assembly
+    ResourceDictionaryLocation.SourceAssembly // look for theme-specific resources in the calling assembly
 )]
+
+// Alternatively, you can use the following code to look for theme-specific resources in a subfolder of the calling assembly
+// [assembly: ThemeInfo(
+//     ResourceDictionaryLocation.SourceAssembly, // look for generic resources in the calling assembly
+//     ResourceDictionaryLocation.Subfolder
+// )]
+
