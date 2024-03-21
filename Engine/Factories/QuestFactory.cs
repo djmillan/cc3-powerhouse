cb4d5b6902852afc4815407b566b9be95314f51a
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-22 05:05:57 +0800

    update QuestFactory

--- Engine/Factories/QuestFactory.cs
+++ Engine/Factories/QuestFactory.cs
@@ -15,8 +15,8 @@ namespace Engine.Factories
 			List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
 			List<ItemQuantity> rewardItems = new List<ItemQuantity>();
 
-			itemsToComplete.Add(new ItemQuantity(100, 3));
-			rewardItems.Add(new ItemQuantity(200, 8));
+			itemsToComplete.Add(new ItemQuantity(9001, 5));
+			rewardItems.Add(new ItemQuantity(1002, 1));
 
 			_quest.Add(new Quest(1, "Collect 3 Crystal Golem Hide from Crystal Caves (West of Rocky Fields) and bring it to the Alchemist (South of Village Outskirts). Rewards: +200 Gold, +100 Exp, and x1 Crystal Potion"));
 
