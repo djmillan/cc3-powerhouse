b8571ea91c1d09554ed595c4f8c9d6a0a128545a
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-22 03:26:45 +0800

    Weapon, ItemQuantity, Weapon.

---
+++ Engine/Models/Quest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace Engine.Models
+{
+	public class Quest
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public  List<ItemQuantity> ItemsToComplete { get; set; }
+		public int RewardExpPoints { get; set; }
+		public int RewardGold { get; set; }
+        public List<ItemQuantity> ItemsToComplete { get; set; }
+
+		public Quest ()
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace Engine.Models
+{
+	public class Weapon : class GameItem
+	{
+		public int MinimumDamage { get; set; }
+		public int MaximumDamage { get; set; }
+		public Weapon(int itemTypeId, string name, int price, string image, int minimumDamage, int maximumDamage):
+			base(itemTypeId, name, price, image, true)
+		{
+			MinimumDamage = minimumDamage;
+			MaximumDamage = maximumDamage;
+		}
+		public new Weapon Clone()
+		{
+			return new Weapon (itemTypeId, name, price, image,minimumDamage, maximumDamage)
+		}
+	}
+}
