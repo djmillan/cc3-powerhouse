b7494435f0c462abc69e3fe043f467b17e032a56
LadyMoral <150553199+LadyMoral@users.noreply.github.com>
2024-03-22 05:00:57 +0800

    update ItemFactory

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+	public static class ItemFactory
+	{
+		private static readonly List<GameItem> class standardGameItems = new List<GameItem>;
+		static ItemFactory()
+		{
+			standardGameItems.Add(new Weapon(1001, "Magical Bow", 1, 1, 2));
+			standardGameItems.Add(new Weapon(1002, "Iron Dagger", 5, 1, 3));
+			standardGameItems.Add(new GameItem(9001, "Wizard Boots", 4);
+			standardGameItems.Add(new GameItem(9002, "Golden Dagger", 1));
+			standardGameItems.Add(new GameItem(9003, "Shield", 10));
+			standardGameItems.Add(new GameItem(9004, "Snipers Chestplate", 8));
+			standardGameItems.Add(new GameItem(9004, "Iron Sword", 6));
+			standardGameItems.Add(new GameItem(9004, "Leather Hood", 2));
+		}
+		public static GameItem CreateGameItem(int itemTypeId)
+		{
+			GameItem standardItem = _standardGameItems.FirstOrDefault(standardGameItems => itemTypeId);
+			if (standardItem == null)
+			{
+				if(standardItem is Weapon)
+				{
+					return (standardItem as Weapon).Clone();
+				}
+				return standardItem.Clone();
+			}
+			return null;
+		}
+	}
+}
---
+++ Engine/Factories/QuestFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace Engine.Factories
+{
+	internal static class QuestFactory
+	{
+		private static readonly List<Quest> _quest = new List<Quest>();
+
+		static QuestFactory()
+		{
+			List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
+			List<ItemQuantity> rewardItems = new List<ItemQuantity>();
+
+			itemsToComplete.Add(new ItemQuantity(100, 3));
+			rewardItems.Add(new ItemQuantity(200, 8));
+
+			_quest.Add(new Quest(1, "Collect 3 Crystal Golem Hide from Crystal Caves (West of Rocky Fields) and bring it to the Alchemist (South of Village Outskirts). Rewards: +200 Gold, +100 Exp, and x1 Crystal Potion"));
+
+			
+		}
+
+		internal static Quest GetQuestID(int id)
+		{ 
+			return _quest.FirstOrDefault(quest => quest.ID == id );
+		}
+
+	}
+}
